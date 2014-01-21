eda86256ab61befa5b9b6f9dd473f0063c851678
shyaminayesh <admin@shyamin.com>
2014-01-21 11:47:42 +0530

    Update import.php

--- import.php
+++ import.php
@@ -21,64 +21,63 @@ $FirID = $FetchIDSet['min'];
 $SecID = $FetchIDSet['max'];
 
 while ( $FirID <= $SecID ):
-		$FetchDetails = mysql_fetch_array(mysql_query("SELECT * FROM client WHERE id='$FirID'"));
-			if ( $FetchDetails['first_name'] !== null ):
-				//echo $FetchDetails['id']."&nbsp;".$FetchDetails['first_name']."</br>";
+	$FetchDetails = mysql_fetch_array(mysql_query("SELECT * FROM client WHERE id='$FirID'"));
+		if ( $FetchDetails['first_name'] !== null ):
+		
+			// GENARATING NEW PASSWORD TO USERS
+			$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+			$pass = array();
+			$alphaLength = strlen($alphabet) - 1;
+			for ($i = 0; $i < 8; $i++) {
+				$n = rand(0, $alphaLength);
+			    	$pass[] = $alphabet[$n];
+			}
 
-				// GENARATING NEW PASSWORD TO USERS
-				$alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
-				$pass = array();
-				$alphaLength = strlen($alphabet) - 1;
-				for ($i = 0; $i < 8; $i++) {
-					$n = rand(0, $alphaLength);
-				    	$pass[] = $alphabet[$n];
-				}
+			$UserPasswords = implode($pass);
 
-				$UserPasswords = implode($pass);
+			// SET DETAILS TO ARRAY
+			$postfields = array();
+			$postfields["username"] = $username;
+			$postfields["password"] = md5($password);
+			$postfields["action"] = "addclient";
+			$postfields["responsetype"] = "json";
+			$postfields["firstname"] = $FetchDetails['first_name'];
+			$postfields["lastname"] = $FetchDetails['last_name'];
+			$postfields["companyname"] = $FetchDetails['company'];
+			$postfields["email"] = $FetchDetails['email'];
+			$postfields["address1"] = $FetchDetails['address_1'];
+			$postfields["address2"] = $FetchDetails['address_2'];
+			$postfields["city"] = $FetchDetails['city'];
+			$postfields["state"] = $FetchDetails['state'];
+			$postfields["postcode"] = $FetchDetails['postcode'];
+			$postfields["country"] = $FetchDetails['country'];
+			$postfields["phonenumber"] = $FetchDetails['phone'];
+			$postfields["password2"] = $UserPasswords;
+			$postfields["customfields"] = base64_encode(serialize(array("1"=>"Yahoo")));
+			$postfields["currency"] = "1";
 
-				// SET DETAILS TO ARRAY
-				$postfields = array();
-				$postfields["username"] = $username;
-				$postfields["password"] = md5($password);
-				$postfields["action"] = "addclient";
-				$postfields["responsetype"] = "json";
-				$postfields["firstname"] = $FetchDetails['first_name'];
-				$postfields["lastname"] = $FetchDetails['last_name'];
-				$postfields["companyname"] = $FetchDetails['company'];
-				$postfields["email"] = $FetchDetails['email'];
-				$postfields["address1"] = $FetchDetails['address_1'];
-				$postfields["address2"] = $FetchDetails['address_2'];
-				$postfields["city"] = $FetchDetails['city'];
-				$postfields["state"] = $FetchDetails['state'];
-				$postfields["postcode"] = $FetchDetails['postcode'];
-				$postfields["country"] = $FetchDetails['country'];
-				$postfields["phonenumber"] = $FetchDetails['phone'];
-				$postfields["password2"] = $UserPasswords;
-				$postfields["customfields"] = base64_encode(serialize(array("1"=>"Google")));
-				$postfields["currency"] = "1";
-
-				$query_string = "";
-				foreach ($postfields AS $k=>$v) $query_string .= "$k=".urlencode($v)."&";
+			$query_string = "";
+			foreach ($postfields AS $k=>$v) $query_string .= "$k=".urlencode($v)."&";
 				 
-				$ch = curl_init();
-				curl_setopt($ch, CURLOPT_URL, $URL);
-				curl_setopt($ch, CURLOPT_POST, 1);
-				curl_setopt($ch, CURLOPT_TIMEOUT, 30);
-				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-				curl_setopt($ch, CURLOPT_POSTFIELDS, $query_string);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
-				curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
-				$jsondata = curl_exec($ch);
-				if (curl_error($ch)) die("Connection Error: ".curl_errno($ch).' - '.curl_error($ch));
-				curl_close($ch);
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $URL);
+			curl_setopt($ch, CURLOPT_POST, 1);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $query_string);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+			$jsondata = curl_exec($ch);
+			if (curl_error($ch)) die("Connection Error: ".curl_errno($ch).' - '.curl_error($ch));
+			curl_close($ch);
 
-				$arr = json_decode($jsondata);
+			$arr = json_decode($jsondata);
 
-				print_r($arr);
+			print_r($arr);
 
-				echo '<pre>'.$FetchDetails['first_name'].'&nbsp;'.$FetchDetails['last_name'].'&nbsp;:&nbsp;'.$FetchDetails['email'].'&nbsp;:&nbsp;'.$UserPasswords.'</pre>';
+			echo '<pre>'.$FetchDetails['first_name'].'&nbsp;'.$FetchDetails['last_name'].'&nbsp;:&nbsp;'.$FetchDetails['email'].'&nbsp;:&nbsp;'.$UserPasswords.'</pre>';
 
-			endif;
+		endif;
 	$FirID++;
 endwhile;
 

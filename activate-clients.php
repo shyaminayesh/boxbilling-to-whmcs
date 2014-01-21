f2873eb1e7ae61f999e25647c44ed2eb96ed9d81
shyaminayesh <admin@shyamin.com>
2014-01-21 21:20:26 +0530

    Create activate-clients.php

---
+++ activate-clients.php
@@ -0,0 +1,30 @@
+<?php
+
+// SET YOUR BOXBILLING DATABASE DETAUKS HERE
+$DBHOST = 'localhost';
+$DBUSER = 'whmcs-database-user';
+$DBPASS = 'whmcs-database-password';
+$DBNAME = 'whmcs-database-name';
+
+// DONT MAKE CHANGES AFTER THIS LINE
+$con = mysql_connect($DBHOST, $DBUSER, $DBPASS) or die (mysql_error());
+$sdb = mysql_select_db($DBNAME) or die (mysql_error());
+
+$QueryIDSet = mysql_query("SELECT MIN(id) AS min, MAX(id) AS max FROM tblclients") or exit(mysql_error());
+$FetchIDSet = mysql_fetch_assoc($QueryIDSet);
+$FirID = $FetchIDSet['min'];
+$SecID = $FetchIDSet['max'];
+
+while ( $FirID <= $SecID ):
+
+	$GetStatus = mysql_fetch_array(mysql_query("SELECT status FROM tblclients WHERE id='$FirID'"));
+	$ClientStatus = $GetStatus['status'];
+
+		if ( $ClientStatus !== 'Active' ):
+			$ActivateQuery = mysql_query("UPDATE $DBNAME.`tblclients` SET `status` = 'Active' WHERE `tblclients`.`id` = $FirID");
+		endif;
+
+	$FirID++;
+endwhile;
+
+?>
